22133a65851c37241a7eb256402daa41457f7b67
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2019-04-03 20:47:12 -0700

    Commit

---
+++ Final Project/Assets/Raul/Scripts/OldPortions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldPortions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		/*if (controller.isGrounded)  This is the Unity tutorial version of player movement and jumping.
+	{
+
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+
+		if (Input.GetButton("Jump"))
+		{
+			moveDirection.y = jumpSpeed;
+		}
+	}
+
+	moveDirection.y -= gravity * Time.deltaTime;
+
+	controller.Move(moveDirection * Time.deltaTime); 
+	
+	}*/
+	}
+}
