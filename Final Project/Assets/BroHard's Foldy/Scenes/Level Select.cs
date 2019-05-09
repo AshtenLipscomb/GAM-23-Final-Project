20baa139aef059ab4534053abfb87ed2c57dffc7
BroItHarder <48001282+BroItHarder@users.noreply.github.com>
2019-05-08 20:58:16 -0700

    scketchy stuff

---
+++ Final Project/Assets/BroHard's Foldy/Scenes/Level Select.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour {
+
+	public int ID { get; set; }
+	public string stage1 { get; set; }
+	public bool Locked {get; set;}
+
+	public Level (int id, string stage1, bool locked)
+	{
+		this.ID =  id;
+		this.LevelName = LevelName;
+		this.Locked = locked;
+	}
+
+	public void Lock()
+	{
+		this.Locked = true;
+	}
+
+	public void Unlock()
+	{
+		this.Locked = false;
+	}
+}
+///NEED MORE///
