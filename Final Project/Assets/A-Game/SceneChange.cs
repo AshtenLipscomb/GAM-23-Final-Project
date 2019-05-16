eed59a1b8cb5de45741d67036a1fb592bb1221bf
BroItHarder <48001282+BroItHarder@users.noreply.github.com>
2019-05-15 21:32:23 -0700

    yeeeeeh

---
+++ Final Project/Assets/A-Game/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ 
+public class SceneChange : MonoBehaviour {
+ 
+    public int num;
+ 
+    // Use this for initialization
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(num);
+            Debug.Log("Scene changed to " + num);
+        }
+    }
+}﻿
