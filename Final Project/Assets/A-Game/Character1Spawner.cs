61345d36b0b996fa95858fc1432c4aa0d01969c7
InsLegacy <48002068+InsLegacy@users.noreply.github.com>
2019-05-29 20:32:10 -0700

    ...

---
+++ Final Project/Assets/A-Game/Character1Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character1Spawner : MonoBehaviour
+{
+
+
+    
+
+    public GameObject[] characterPrefabs;
+
+	// Use this for initialization
+	void Start ()
+    {
+        int selectedCharacterIndex = PlayerPrefs.GetInt("player1.character", 0);
+
+        Instantiate(characterPrefabs[selectedCharacterIndex], transform.position, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Final Project/Assets/A-Game/Character2Select.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Character2Select : MonoBehaviour
+{
+    void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void P2SelectCharacter1()
+    {
+        PlayerPrefs.SetInt("player2.character", 0);
+    }
+
+    public void P2SelectCharacter2()
+    {
+        PlayerPrefs.SetInt("player2.character", 1);
+    }
+
+    public void P2SelectCharacter3()
+    {
+        PlayerPrefs.SetInt("player2.character", 2);
+    }
+
+    public void P2SelectCharacter4()
+    {
+        PlayerPrefs.SetInt("player2.character", 3);
+    }
+
+    public void P2SelectCharacter5()
+    {
+        PlayerPrefs.SetInt("player2.character", 4);
+    }
+
+    public void P2SelectCharacter6()
+    {
+        PlayerPrefs.SetInt("player2.character", 5);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
